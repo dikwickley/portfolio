0677bebb63f7a959017e58053b4ea784a5630956
Aniket Singh Rawat <sprx077@gmail.com>
2021-07-20 13:31:45 +0530

    updated project list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,17 @@
 export const projects = [
   {
+    name: "xamplan",
+    about: "Tool for making a study plan for competitive exams like GATE.",
+    github: "-",
+    url: "https://xamplan.herokuapp.com/",
+  },
+  {
+    name: "recruitment",
+    about: "Full fledged job portal for everyone",
+    github: "-",
+    url: "https://online-recruitment.herokuapp.com/",
+  },
+  {
     name: "thinktactoe",
     about: "A Multiplayer Tic tac toe game with video calling.",
     github: "https://github.com/DiKwickley/thinktactoe",
@@ -16,5 +28,24 @@ export const projects = [
     about: "Reddit bot that spreads positivity through NLP",
     github: "https://github.com/cryptus-neoxys/merry-cacti",
     url: "https://merry-cacti.vercel.app/",
+  },
+  {
+    name: "recipees",
+    about: "An app that finds recipes based on ingredients",
+    github: "https://github.com/cryptus-neoxys/recipes",
+    url: "https://recipes-appp.vercel.app/",
+  },
+  {
+    name: "pose-bot",
+    about:
+      "A simple utility tool that monitors your posture while you are working and offer corrections.",
+    github: "https://github.com/cryptus-neoxys/pose-bot",
+    url: "https://pose-bot.vercel.app/",
+  },
+  {
+    name: "crypto-bot",
+    about: "Dashboard for crypto trading bot.",
+    github: "-",
+    url: "http://bitbot69.herokuapp.com/",
   },
 ];
